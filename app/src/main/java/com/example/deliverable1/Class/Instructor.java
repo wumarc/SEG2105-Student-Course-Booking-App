a6b25366b3fabf30141ec159348f96f32d168e08
imaginaryrationalpi <32423253+imaginaryrationalpi@users.noreply.github.com>
2021-06-17 01:31:06 -0400

    Creating a brand new project

--- app/src/main/java/com/example/deliverable1/Class/Instructor.java
+++
@@ -1,9 +0,0 @@
-package com.example.deliverable1.Class;
-
-public class Instructor extends User {
-
-    public Instructor(String email, String password, String firstName, String lastName, String userType) {
-        super(email, password, firstName, lastName, userType);
-    }
-
-}
--- app/src/main/java/com/example/deliverable1/Class/Student.java
+++
@@ -1,9 +0,0 @@
-package com.example.deliverable1.Class;
-
-public class Student extends User {
-
-    public Student(String email, String password, String firstName, String lastName, String userType) {
-        super(email, password, firstName, lastName, userType);
-    }
-
-}
